9301da4e7fffcf029c81646cdc302a4f168e36d8
Takayuki Shimada <taka@tsmd.jp>
2020-12-11 02:42:52 +0900

    Accordion をリファクタ

--- my_accordion.js
+++ my_accordion.js
@@ -14,158 +14,134 @@
  *      </div>
  *    </div>
  *  </section>
- *  
+ *
  *  ...
- *  
+ *
  * </div>
  *
- * @codepen https://codepen.io/tak-dcxi/pen/yLaaJYj 
+ * @codepen https://codepen.io/tak-dcxi/pen/yLaaJYj
  **/
 
+const defaultOptions = {
+  // アコーディオンのパネルの開閉時のtransitionを指定
+  timingFunction: 'ease-out',
+  duration: '.3s',
+  // 複数開けるようにするか
+  openMultiple: false,
+};
+
 export default class Accordion {
-  constructor(setting) {
-    const defaultSetting = {
-      // アコーディオンコンポーネントに付与するclass
-      component: '.js-accordion',
-      // アコーディオンのタブに付与するclass
-      tabs: '.js-accordion-tab',
-      // アコーディオンのパネルに付与するclass
-      panels: '.js-accordion-panel',
-      // アコーディオンのパネルの開閉時のtransitionを指定
-      toggleAnimation: 'max-height ease-out .3s',
-      // 複数開けるようにするか
-      openMultiple: false,
-      // 初期化時に最初のパネルを開いておくか
-      openFirstItem: true,
-    };
+  constructor(element, options) {
+    const mergedOptions = Object.assign({}, defaultOptions, options);
 
-    this.setting = Object.assign(defaultSetting, setting);
+    // 必須オプションをチェックする
+    if (!options.tabs) {
+      throw TypeError('tabs オプションは必須です');
+    }
+    if (!options.panels) {
+      throw TypeError('panels オプションは必須です');
+    }
 
-    Object.keys(this.setting).forEach(key => {
-      this[key] = this.setting[key];
-    });
+    // タブとパネルを取ってくる
+    const tabs = Array.from(element.querySelectorAll(options.tabs));
+    const panels = Array.from(element.querySelectorAll(options.panels));
 
-    this.selector = {
-      component: Array.from(document.querySelectorAll(this.component)),
-      tabs: Array.from(document.querySelectorAll(this.tabs)),
-      panels: Array.from(document.querySelectorAll(this.panels)),
-    };
+    // イベントハンドラを設定する
+    const subscriptions = [
+      ...tabs.map((tab) => attachEvent(tab, 'click', this.handleTabClick.bind(this))),
+      attachEvent(window, 'resize', this.handleResize.bind(this)),
+    ]
+
+    this.element = element;
+    this.tabs = tabs;
+    this.panels = panels;
+    this.options = mergedOptions;
+    this.subscriptions = subscriptions;
+    this.expanded = new Set();
+
+    this.prepareAttributes();
   }
 
-  // 初期化
-  init() {
-    // アコーディオンコンポーネントがページ上に無い場合は処理を終える
-    if (!this.selector.component.length) {
-      return;
-    }
+  destroy() {
+    this.subscriptions.forEach((subscription) => {
+      subscription.unsubscribe();
+    })
+  }
 
-    // アコーディオンコンポーネントに属性を付与する
-    this.setAttributes();
+  handleTabClick(event) {
+    const tab = event.currentTarget;
+    const tabIndex = this.tabs.indexOf(tab);
+    this.toggleItem(tabIndex, !this.expanded.has(tabIndex));
+    event.preventDefault();
+  }
 
-    // 1項目目を表示する設定をしている場合
-    if (this.openFirstItem) {
-      this.showFirstItem();
-    }
-
-    // タブがクリックされた際の処理
-    this.toggleItem();
-    
-    // 画面の幅に変動があった際の処理
+  handleResize() {
     this.adjustHeight();
   }
 
-  // アコーディオンコンポーネントに属性を付与する処理
-  setAttributes() {
+  prepareAttributes() {
+    const randomId = 'accordion-' + Math.random().toString(36).slice(2);
+
     // アコーディオンコンポーネントのタブに付与する属性
-    this.selector.tabs.forEach((tab, number) => {
-      tab.setAttribute('id', `accordion-tab-number-${(number + 1)}`);
-      tab.setAttribute('aria-expanded', false);
-      tab.setAttribute('aria-controls', `accordion-panel-number-${(number + 1)}`);
+    this.tabs.forEach((tab, index) => {
+      tab.setAttribute('id', `${randomId}-tab-${index}`);
+      tab.setAttribute('aria-expanded', "false");
+      tab.setAttribute('aria-controls', `${randomId}-panel-${index}`);
     });
 
     // アコーディオンコンポーネントのパネルに付与する属性
-    this.selector.panels.forEach((panel, number) => {
-      panel.setAttribute('id', `accordion-panel-number-${(number + 1)}`);
-      panel.setAttribute('aria-hidden', true);
-      panel.setAttribute('aria-labelledby', `accordion-tab-number-${(number + 1)}`);
+    this.panels.forEach((panel, index) => {
+      panel.setAttribute('id', `${randomId}-panel-${index}`);
+      panel.setAttribute('aria-hidden', "true");
+      panel.style.boxSizing = 'border-box';
       panel.style.overflow = 'hidden';
       panel.style.maxHeight = '0px';
     });
   }
 
-  // 最初の項目を表示する
-  showFirstItem() {
-    const firstItemsTab = this.selector.tabs[0];
-    const firstItemsPanel = this.selector.panels[0];
-    const firstItemsContentsHeight = firstItemsPanel.children[0].clientHeight;
-    
-    firstItemsTab.setAttribute('aria-expanded', true);
-    firstItemsPanel.setAttribute('aria-hidden', false);
-    firstItemsPanel.style.maxHeight = firstItemsContentsHeight + 'px';
+  toggleItem(itemIndex, expand, {noTransition = false} = {}) {
+    const isItemExpanded = this.expanded.has(itemIndex);
+
+    if (expand === isItemExpanded) {
+      return;
+    }
+
+    const updateItemAttribute = (itemIndex, expand) => {
+      const targetTab = this.tabs[itemIndex];
+      const targetPanel = this.panels[itemIndex];
+      targetTab.setAttribute('aria-expanded', String(expand));
+      targetPanel.setAttribute('aria-hidden', String(!expand));
+      targetPanel.style.maxHeight = expand ? targetPanel.children[0].clientHeight + 'px' : '0px';
+      targetPanel.style.visibility = expand ? 'visible' : 'hidden';
+      targetPanel.style.transition = noTransition ?
+        '' :
+        `max-height ${this.options.timingFunction} ${this.options.duration}, visibility ${this.options.duration}`;
+      this.expanded[expand ? 'add' : 'delete'](itemIndex);
+    }
+
+    // 複数選択可能の設定がされていない時は開いているパネルをすべて閉じる
+    if (!this.options.openMultiple && !isItemExpanded) {
+      this.expanded.forEach((index) => updateItemAttribute(index, false));
+    }
+
+    updateItemAttribute(itemIndex, expand);
   }
 
-  // タブがクリック・タップされた時の処理
-  toggleItem() {
-    this.selector.tabs.forEach(tab => {
-      tab.addEventListener('click', clickedTab => {
-        const targetTab = document.getElementById(clickedTab.target.id);
-        const targetPanel = document.querySelector(`[aria-labelledby='${clickedTab.target.id}']`);
-        const isItemExpanded = targetTab.getAttribute('aria-expanded') === 'true';
-        
-        clickedTab.stopPropagation();
-        clickedTab.preventDefault();
-
-        // 複数選択可能の設定がされていない時は開いているパネルをすべて閉じる
-        if (!this.openMultiple && !isItemExpanded) {
-          this.closeAllItems();
-        }
-        
-        // 開閉時にスライドを行う
-        if (!isItemExpanded) {
-          const contentHeight = targetPanel.children[0].clientHeight;
-          targetPanel.style.maxHeight = contentHeight + 'px';
-        } else {
-          targetPanel.style.maxHeight = '0px';
-        }
-        
-        if (this.toggleAnimation !== '' || this.toggleAnimation !== undefined) {
-          targetPanel.style.transition = this.toggleAnimation;
-        }
-
-        // 開閉時にWAI-ARIAの属性値を切り替える
-        targetTab.setAttribute('aria-expanded', !isItemExpanded);
-        targetPanel.setAttribute('aria-hidden', isItemExpanded);
-      });
-    });
-  }
-
-  // すべての項目を閉じる
-  closeAllItems() {
-    this.selector.tabs.forEach(tab => {
-      const targetTab = tab.getAttribute('id');
-      const targetPanel = document.querySelector(`[aria-labelledby="${targetTab}"]`);
-      
-      tab.setAttribute('aria-expanded', false);
-      
-      targetPanel.setAttribute('aria-hidden', true);
-      targetPanel.style.maxHeight = '0px';
-    });
-  }
-  
-  // レスポンシブで画面幅が変動した時に高さを再計算する
   adjustHeight() {
-    this.selector.panels.forEach(panel => {
-      window.addEventListener('resize', () => {
-        const isExpanded = panel.style.maxHeight !== '0px';
-        
-        // 開いているパネルの高さを再計算する
-        if (isExpanded) {
-          const resizedHeight = panel.children[0].clientHeight;
-          panel.style.maxHeight = resizedHeight + 'px';
-        }
-        
-        return;
-      });
+    // 開いているパネルの高さを再計算する
+    this.expanded.forEach((index) => {
+      const panel = this.panels[index];
+      const resizedHeight = panel.children[0].clientHeight;
+      panel.style.maxHeight = resizedHeight + 'px';
     });
   }
-};
+}
+
+function attachEvent(element, event, handler, options) {
+  element.addEventListener(event, handler, options);
+  return {
+    unsubscribe() {
+      element.removeEventListener(event, handler);
+    }
+  }
+}
